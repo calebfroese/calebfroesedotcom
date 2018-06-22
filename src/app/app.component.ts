85a2d5febb74fe418da99655e26279ef563b4414
Caleb Froese <caleb.froese@gmail.com>
2018-06-22 16:22:42 +0930

    Update to include version number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  version: string;
+
+  ngOnInit() {
+    const createdAt = dayjs(new Date('1997-10-07'));
+    const updatedAt = dayjs();
+    const years = updatedAt.diff(createdAt, 'year');
+    const months = updatedAt.subtract(years, 'year').diff(createdAt, 'month');
+    const days = updatedAt
+      .subtract(years, 'year')
+      .subtract(months, 'month')
+      .diff(createdAt, 'day');
+    this.version = [years, months, days].join('.');
+  }
 }
