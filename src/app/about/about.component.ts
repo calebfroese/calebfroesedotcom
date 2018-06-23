db82716149f6c550ac2d9b3103d0fbcc74c57696
Caleb Froese <caleb.froese@gmail.com>
2018-06-23 22:02:06 +0930

    Remove version

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as anime from 'animejs';
-import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'app-about',
@@ -8,20 +7,8 @@ import * as dayjs from 'dayjs';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-  version: string;
   ngOnInit() {
     const { innerWidth: x, innerHeight: y } = window;
-
-    const createdAt = dayjs(new Date('1997-10-07'));
-    const updatedAt = dayjs();
-    const years = updatedAt.diff(createdAt, 'year');
-    const months = updatedAt.subtract(years, 'year').diff(createdAt, 'month');
-    const days = updatedAt
-      .subtract(years, 'year')
-      .subtract(months, 'month')
-      .diff(createdAt, 'day');
-    this.version = [years, months, days].join('.');
-
     anime({
       targets: '.image',
       translateX: -x / 2,
