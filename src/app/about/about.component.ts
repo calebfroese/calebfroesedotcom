68afecbaedb294c2c9784112ed6617432d35115c
Caleb Froese <caleb.froese@gmail.com>
2018-06-24 14:58:47 +0930

    Remove animations

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as anime from 'animejs';
 
 @Component({
   selector: 'app-about',
@@ -7,24 +6,5 @@ import * as anime from 'animejs';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-  ngOnInit() {
-    const { innerWidth: x, innerHeight: y } = window;
-    const duration = 2000;
-    anime({
-      targets: '.image',
-      translateY: -y / 3,
-      rotateX: '0.02turn',
-      direction: 'reverse',
-      duration,
-      easing: 'easeInOutBack'
-    });
-    anime({
-      targets: '.image-abstract',
-      translateY: y / 3,
-      rotateX: '-0.02turn',
-      direction: 'reverse',
-      duration,
-      easing: 'easeInOutBack'
-    });
-  }
+  ngOnInit() {}
 }
