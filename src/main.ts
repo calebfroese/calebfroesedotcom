756ae969636b2e109e5f0cb07826612ea9a22a93
Caleb Froese <caleb.froese@gmail.com>
2019-05-26 18:26:37 +0930

    Implement google analytics
    Create production configuration and use null in development.
    This allows visibility of active users and website hits.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as googleAnalytics from 'google-analytics-js';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -7,6 +8,8 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+
+googleAnalytics(environment.analytics.code, environment.analytics.domain);
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
